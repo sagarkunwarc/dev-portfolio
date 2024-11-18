1819beefb363c69ae2f6309a4f5ef14a53ba1993
sagarkunwar <sagar.kunwar24@gmail.com>
2024-11-17 20:29:51 -0500

    mew skills add to list

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -3,11 +3,36 @@ export default function Skills() {
     <section className="skills">
       <h2>Skills</h2>
       <ul>
-        <li>HTML</li>
-
-        <li>CSS</li>
+        <li>Java</li>
 
         <li>JavaScript</li>
+
+        <li>SQL</li>
+
+        <li>C#</li>
+
+        <li>Spring</li>
+
+        <li>Spring Boot</li>
+
+        <li>React</li>
+
+        <li>Next JS</li>
+
+        <li>Express</li>
+
+        <li>Redis</li>
+
+        <li>GCP</li>
+
+        <li>AWS</li>
+
+        <li>Git</li>
+
+        <li>Tailwind</li>
+
+      
+
 
         {/* Add more skills */}
       </ul>
